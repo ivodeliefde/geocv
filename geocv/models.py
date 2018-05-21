f32c971c75ae9598c300a231c4aa45f9c1b160b0
Florian <fwfichtner@gmail.com>
2018-05-21 19:17:11 +0200

    create models

--- geocv/models.py
+++ geocv/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# something like this to create the model?
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+class Job(models.Model):
+    CATEGORY = (('j','job'),('e','education'),)
+    category = models.CharField(max_length=1, choices=CATEGORY)
+    last_name = models.ForeignKey(Person, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    description = models.CharField(max_length=90)
+    location = models.PointField()
